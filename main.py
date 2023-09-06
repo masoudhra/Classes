596000ed54e1bbbe25de926887bc0c03a7dcf974
Masoud Ramezani <masoudhra@gmail.com>
2023-09-06 12:28:33 +1200

    work in oop and clases in python

---
+++ main.py
@@ -0,0 +1,10 @@
+from SoccerPlayer import Player
+
+player1 = Player("Masoud", 33, "Perspolis", False)
+player2 = Player("OstadAsadi", 55, "Zob Ahan", True)
+
+
+# player2.injured = False
+
+player1.get_status()
+player2.get_status()
